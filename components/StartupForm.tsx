a6393263f60c3e646d9b4a61e0dcf32e83c6bf7c
Mustafa <yusufmustufa@gmail.com>
2024-11-28 18:32:35 +0530

    Completed Create Form Actions

--- components/StartupForm.tsx
+++ components/StartupForm.tsx
@@ -10,13 +10,13 @@ import { formSchema } from "@/lib/validation";
 import { z } from "zod";
 import { useToast } from "@/hooks/use-toast";
 import { useRouter } from "next/navigation";
-//import { createPitch } from "@/lib/actions";
+import { createPitch } from "@/lib/action";
 
 const StartupForm = () => {
   const [errors, setErrors] = useState<Record<string, string>>({});
   const [pitch, setPitch] = useState("");
   const { toast } = useToast();
-  // const router = useRouter();
+  const router = useRouter();
 
   const handleFormSubmit = async (prevState: any, formData: FormData) => {
     try {
@@ -29,20 +29,19 @@ const StartupForm = () => {
       };
 
       await formSchema.parseAsync(formValues);
-      console.log(formValues);
 
-    //   const result =   await createPitch(prevState, formData, pitch);
+      const result = await createPitch(prevState, formData, pitch);
 
-    //   if (result.status == "SUCCESS") {
-    //     toast({
-    //       title: "Success",
-    //       description: "Your startup pitch has been created successfully",
-    //     });
+      if (result.status == "SUCCESS") {
+        toast({
+          title: "Success",
+          description: "Your startup pitch has been created successfully",
+        });
 
-    //     router.push(`/startup/${result._id}`);
-    //   } 
+        router.push(`/startup/${result._id}`);
+      }
 
-    //  return result;
+      return result;
     } catch (error) {
       if (error instanceof z.ZodError) {
         const fieldErorrs = error.flatten().fieldErrors;
